9e3c67a3475fd11f1c0de20f32e91fb2217936e0
sheyang <sheyang@forchange.tech>
2019-01-09 15:09:23 +0800

    finish:缓存代理

--- proxy-mode.js
+++ proxy-mode.js
@@ -2,6 +2,8 @@
 //保护代理：代理对象可帮主体对象过滤点一些请求，控制不同权限的对象对目标对象的访问
 //虚拟代理：把一些开销很大的对象，延迟到真正需要它的时候才去创建。
 //代理模式 xiaoming---->proxyObj---->AMM
+
+//单一职责原则：就一个类（也包括对象和函数等）而言，应该仅有一个引起它变化的原因，如果一个对象承担着多项职责，就意味着这个对象将变得巨大。
 var proxyObj={
   receiveFlower:function(){
     bodyObj.listenGoodMood(function(){   // 监听主体对象状态
@@ -26,7 +28,7 @@ var proxyImage=(function(){
   var img = new Image
   img.onload=function(){
     myImage.setsrc(this.src)
-  }
+  }   //解除耦合性把图片预加载与设置img节点的src分开 符合单一职责原则
   return {
     setSrc: function(src){
       myImage.setSrc('file://c/.././')
@@ -37,4 +39,24 @@ var proxyImage=(function(){
 
 proxyImage.setSrc('图片地址')
 
-//单一职责原则：就一个类（也包括对象和函数等）而言，应该仅有一个引起它变化的原因，如果一个对象承担看多项职责，就意味着这个对象将变得巨大。
+/**
+ * 缓存代理
+ */
+var mult=function(){
+  var a= 1 ;
+  for(var i=0,len=arguments.length;i<len;i++){
+    a = a * arguments[i]
+  }
+  return a 
+}
+var proxyMult = (function(){
+  var cache={}
+  return function(){
+    var args = Array.prototype.join.call(arguments,',') // arguments ：是一个对应于传递给函数的参数的类数组对象
+    if(cache[args]){   //检索缓存是否存在
+      return cache[args]
+    }
+    return cache[args] = mult.apply(this,arguments) //每一次运算结果缓存起来
+  }
+})()
+proxyMult(1,2,3,4) //输出24
